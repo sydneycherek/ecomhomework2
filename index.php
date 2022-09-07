09798a0fc885d2b313c90a7841adb0e217bdac22
sydneycherek <78374649+sydneycherek@users.noreply.github.com>
2022-09-07 14:04:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php require_once("header.php"); ?>
 <h1> My Website </h1>
 
+<form action="handlepost.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
 </html>
