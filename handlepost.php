41081cba55c94db5f0af4689a3ca889977e2c446
sydneycherek <78374649+sydneycherek@users.noreply.github.com>
2022-09-07 14:35:58 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -10,7 +10,7 @@
     <h1>Post Response</h1>
     Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
-    <a class="btn btn-primary" href="page2.html" role="button"> Page 2 </a>
+    <a class="btn btn-primary" href="index.php" role="button"> Back </a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
 </html>
