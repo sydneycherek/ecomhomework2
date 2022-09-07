e9d1d9280c8b3a7f0c35ffca1b4b621d01dcaab0
sydneycherek <78374649+sydneycherek@users.noreply.github.com>
2022-09-07 14:33:51 -0500

    Update get.php

--- get.php
+++ get.php
@@ -5,7 +5,7 @@ Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
 </form>
-<a class="btn btn-primary" href="page2.html" role="button">Page 2</a>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
 </html>
