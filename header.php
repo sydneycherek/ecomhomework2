af0b149cc8ad1ef5565ac03a7fab1fc780f27268
sydneycherek <78374649+sydneycherek@users.noreply.github.com>
2022-09-07 14:29:58 -0500

    Update header.php

--- header.php
+++ header.php
@@ -17,10 +17,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Post</a>
+          <a class="nav-link active" aria-current="page" href="post.php">Post</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Get</a>
+          <a class="nav-link" href="get.php">Get</a>
         </li>
         
     </div>
